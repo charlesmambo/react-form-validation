0e7e48ad57c7ba14e425d43f81cc661a24ac9fc5
Charles Mambo <chylah11st@gmail.com>
2023-08-03 07:11:20 +0200

    Complete jsx boilerplate

--- src/App.js
+++ src/App.js
@@ -4,20 +4,51 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <fieldset>
+        <h2>Sign Up</h2>
+
+        <div className='Field'>
+          <label>
+            First name
+          </label>
+          <input placeholder='First name'/>
+        </div>
+
+        <div className='Field'>
+          <label>
+            Last name
+          </label>
+          <input placeholder='Last name'/>
+        </div>
+
+        <div className='Field'>
+          <label>
+           Email Address
+          </label>
+          <input placeholder='Email address'/>
+        </div>
+
+        <div className='Field'>
+          <label>
+           Password
+          </label>
+          <input placeholder='Password'/>
+        </div>
+
+        <div className="Field"> 
+        <label> 
+          Role <sup>*</sup> 
+        </label> 
+        <select> 
+          <option value="role">Role</option> 
+          <option value="individual">Individual</option> 
+          <option value="business">Business</option> 
+        </select> 
+        </div> 
+
+        </fieldset>
+      </form>
     </div>
   );
 }
